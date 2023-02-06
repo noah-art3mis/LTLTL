c3c0e17bc743b560c6c692a9aba77ed410e1f8fa
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-02-06 18:01:55 -0300

    level design

--- credits.gd
+++ credits.gd
@@ -4,6 +4,8 @@
 	#3dcc
 	#dialogic
 
+#hdris 
+#
 
 #music
 #
--- levels/level_10/grass.gd
+++ levels/level_10/grass.gd
@@ -2,3 +2,12 @@ extends MeshInstance
 
 func _on_Area_body_entered(body):
 	$StaticBody2/CollisionShape.disabled = true
+	$Timer.start()
+	
+signal end
+func _ready():
+	connect("end", get_parent().get_node("LevelManager"), "_on_DialogueHandler_next_level")
+	
+func _on_Timer_timeout():
+	emit_signal("end")
+	
